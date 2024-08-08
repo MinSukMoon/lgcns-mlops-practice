f102b40c0a0179e2c73dd4ecf8ed425065e6ca9d
RM <41184856+MinSukMoon@users.noreply.github.com>
2024-08-08 07:24:34 +0000

    asdf

--- batch_prediction.sh
+++ batch_prediction.sh
@@ -1,4 +1,4 @@
 BASH_ENV=~/.bashrc
 ROOT_PATH=/workspaces/lgcns-mlops-practice
 
-$ROOT_PATH/.venv/bin/python $ROOT_PATH/batch_prediction.py >> $ROOT_PATH/cron.log 2>&1
+$ROOT_PATH/code_snippets/.venv/bin/python $ROOT_PATH/batch_prediction.py >> $ROOT_PATH/cron.log 2>&1
